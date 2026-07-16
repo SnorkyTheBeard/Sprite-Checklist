03a0a75d92426c1355b6cbd4f3a28e86b7fb7614
SnorkyTheBeard <aaron.taylor.jones@gmail.com>
2026-07-16 07:22:37 -0500

    Delete published-design.js

--- published-design.js
+++
@@ -1,3 +0,0 @@
-// This file holds the public default design. Use "Download public design" in
-// Edit Mode, then replace this file in GitHub to publish your finished changes.
-window.PUBLISHED_DESIGN = {};
